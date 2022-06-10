e49d3c8841357d844d176e142900f7edf8494607
kairui.wang <kairui.wang@horizon.ai>
2022-06-11 00:47:04 +0800

    feature(copyright): [cr_id_skip] Add copyright

--- performance_test/src/communication_abstractions/rclcpp_callback_communicator.hpp
+++ performance_test/src/communication_abstractions/rclcpp_callback_communicator.hpp
@@ -1,4 +1,5 @@
 // Copyright 2017 Apex.AI, Inc.
+// Copyright (c) 2022，Horizon Robotics.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
